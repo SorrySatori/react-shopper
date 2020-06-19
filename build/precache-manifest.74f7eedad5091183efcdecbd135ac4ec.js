2b4649fb05cdd7a36a612e6d03df6586337efbd9
SorrySatori <64067870+SorrySatori@users.noreply.github.com>
2020-06-19 12:14:12 +0200

    Add files via upload

---
+++ build/precache-manifest.74f7eedad5091183efcdecbd135ac4ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb868508d1fdb45a71f73756c7d13e2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c561d8433b82d6703f7",
+    "url": "/static/css/main.f5f7ec7e.chunk.css"
+  },
+  {
+    "revision": "f14690d948d2745346c9",
+    "url": "/static/js/2.c04e9369.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c04e9369.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c561d8433b82d6703f7",
+    "url": "/static/js/main.d349a9fd.chunk.js"
+  },
+  {
+    "revision": "cd0166dcd278122b405b",
+    "url": "/static/js/runtime-main.f267015a.js"
+  }
+]);
